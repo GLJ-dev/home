53e395a3426fcb995bf14d023a88f8b5d3454a65
ImLukas <glj.dev@icloud.com>
2021-11-07 19:20:13 +0100

    ready

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home'
 import Timeline from './pages/Timeline'
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import {HashRouter as Router, Route, Switch} from "react-router-dom"
 
 
 function App() {
   return (
   <div className="App"> 
+    aaaaaaaaaaaaaaaaaaaaaaaa
     <Router>
-      AAAAAA
       <Navbar />
-      <Route path="/home" exact component={Home} />
       <Switch>
+        <Route path="/home" exact component={Home} />
         <Route path="/Timeline" exact component={Timeline} />
       </Switch>
     </Router>
